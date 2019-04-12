658fda74f087223acd8c19eb650e3beab93b1a3c
ITDevStar <itdevstar@hotmail.com>
2019-04-12 08:51:47 +0300

    image lib merge(opencv)

--- FFMpegFilterApp/android/app/src/main/java/com/ffmpegfilterapp/MainApplication.java
+++ FFMpegFilterApp/android/app/src/main/java/com/ffmpegfilterapp/MainApplication.java
@@ -3,13 +3,24 @@ package com.ffmpegfilterapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 
+import com.reactlibrary.RNOpenCvLibraryPackage;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+
 import java.util.Arrays;
 import java.util.List;
+
+import org.opencv.android.OpenCVLoader;
+
+import android.util.Log;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,7 +33,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new SvgPackage(),
+            new RNCameraPackage(),
+            new RNOpenCvLibraryPackage()
       );
     }
 
@@ -37,9 +51,40 @@ public class MainApplication extends Application implements ReactApplication {
     return mReactNativeHost;
   }
 
+  private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+    @Override
+    public void onManagerConnected(int status) {
+      switch (status) {
+        case LoaderCallbackInterface.SUCCESS:
+        {
+          Log.i("OpenCV", "OpenCV loaded successfully");
+        } break;
+        default:
+        {
+          super.onManagerConnected(status);
+        } break;
+      }
+    }
+  };
+
   @Override
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+
+    if (!OpenCVLoader.initDebug()) {
+      Log.d("OpenCv", "Error while init");
+    }
+  }
+
+  public void onResume()
+  {
+    if (!OpenCVLoader.initDebug()) {
+      Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+      OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+    } else {
+      Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+      mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+    }
   }
 }
